4d1f33de2da4752c450b4a4d9a049f29a7a3ba2b
tirea <tirea@tirearadio.com>
2016-06-20 23:59:23 +0000

    Affix parsing still requires work

--- util/affix.go
+++ util/affix.go
@@ -14,22 +14,26 @@
 
 //	This util library contains functions for handling affixed words
 
+// COMPLETE LOGIC OVERHAUL REQUIRED
+
 package util
 
 import (
-	"strings"
 	"regexp"
+	"strings"
 )
 
-var nPrefixRe string = "(fì|tsa|me|pxe|ay|fay|tsay|fne|sna|munsna|fra|fray|pe|pem|pep|pay)?"
-var vPrefixRe string = "(tsuk|ketsuk|tì)?"
-var adjPrefixRe string = "(a|nì|ke|kel|kele)?"
-var advPrefixRe string = "(nìk)?"
-var nSuffixRe string = "(ìl|l|ti|it|t|ru|ur|r|yä|ä|ìri|ri|ya|fkeyk|o|pe|tsyìp|am|ay|y|äo|eo|fa|few|fpi|ftu|ftumfa|hu|io|ìlä|kam|kay|krrka|ka|kxamlä|lisre|lok|luke|mìkam|mì|mungwrr|na|nemfa|ne|nuä|pxaw|pxel|pximaw|maw|pxisre|rofa|ro|sìn|sko|sre|tafkip|takip|fkip|kip|talun|ta|teri|uo|vay|wä|yoa)?"
-var numSuffixRe string = "(ve)?"
-var adjSuffixRe string = "(a|pin)?"
-var vSuffixRe string = "(yu|tswo)?"
+var nPrefixes = []string{"fì", "tsa", "me", "pxe", "ay", "fay", "tsay", "fne", "sna", "munsna", "fra", "fray", "pe", "pem", "pep", "pay"}
+var vPrefixes = []string{"tsuk", "ketsuk", "tì"}
+var adjPrefixes = []string{"a", "nì", "ke", "kel", "kele"}
+var advPrefix string = "nìk"
+var nSuffixes = []string{"ìl", "l", "ti", "it", "t", "ru", "ur", "r", "yä", "ä", "ìri", "ri", "ya", "fkeyk", "o", "pe", "tsyìp", "am", "ay", "y", "äo", "eo", "fa", "few", "fpi", "ftu", "ftumfa", "hu", "io", "ìlä", "kam", "kay", "krrka", "ka", "kxamlä", "lisre", "lok", "luke", "mìkam", "mì", "mungwrr", "na", "nemfa", "ne", "nuä", "pxaw", "pxel", "pximaw", "maw", "pxisre", "rofa", "ro", "sìn", "sko", "sre", "tafkip", "takip", "fkip", "kip", "talun", "ta", "teri", "uo", "vay", "wä", "yoa"}
+var numSuffix string = "ve"
+var adjSuffixes = []string{"a", "pin"}
+var vSuffixes = []string{"yu", "tswo"}
 var lentable = map[string]string{}
+
+//var result = [][]string{}
 
 func init() {
 	lentable["kx"] = "k"
@@ -48,22 +52,24 @@ func Infix(w string, inf_re string) [][]string {
 	var orig string = inf_re
 	var result [][]string
 
-	inf_re = strings.Replace(inf_re,"<1>",Text("INFIX_0"),1)
-	inf_re = strings.Replace(inf_re,"<2>",Text("INFIX_1"),1)
-	inf_re = strings.Replace(inf_re,"<3>",Text("INFIX_2"),1)
-	orig = strings.Replace(orig,"<1>","",1)
-	orig = strings.Replace(orig,"<2>","",1)
-	orig = strings.Replace(orig,"<3>","",1)
+	inf_re = strings.Replace(inf_re, "<1>", Text("INFIX_0"), 1)
+	inf_re = strings.Replace(inf_re, "<2>", Text("INFIX_1"), 1)
+	inf_re = strings.Replace(inf_re, "<3>", Text("INFIX_2"), 1)
+	orig = strings.Replace(orig, "<1>", "", 1)
+	orig = strings.Replace(orig, "<2>", "", 1)
+	orig = strings.Replace(orig, "<3>", "", 1)
 
 	re, err := regexp.Compile(inf_re)
-	if err != nil { panic(err) }
+	if err != nil {
+		panic(err)
+	}
 
 	// pull out all infixes used and stash them in the result array
 	result = re.FindAllStringSubmatch(w, -1)
 
 	//replace the infixed verb in each array 1st position with root word
 	if len(result) > 0 {
-		result[0][0] = strings.Replace(result[0][0],result[0][0],orig,1)
+		result[0][0] = strings.Replace(result[0][0], result[0][0], orig, 1)
 	}
 
 	return result
@@ -72,32 +78,10 @@ func Infix(w string, inf_re string) [][]string {
 // handles other types of words having prefixes
 func Prefix(w string, pre_re string, pos string) [][]string {
 
-	var orig string = pre_re
 	var result [][]string
 
-	switch pos {
-		case "n.":
-			pre_re = nPrefixRe + pre_re
-		case "v.", "vin.", "vim.", "vtr.", "vtrm.":
-			pre_re = vPrefixRe + pre_re
-		case "adj.":
-			pre_re = adjPrefixRe + pre_re
-		case "adv.":
-			pre_re = advPrefixRe + pre_re
-		default:
-			break
-	}
-
-	re, err := regexp.Compile(pre_re)
-	if err != nil { panic(err) }
-
-	result = re.FindAllStringSubmatch(w, -1)
-
-	if len(result) > 0 {
-		result [0][0] = strings.Replace(result[0][0],result[0][0],orig,1)
-	}
-
 	return result
+
 }
 
 // handles other types of words having suffixes
@@ -108,6 +92,7 @@ func Suffix(w string, suf_re string, pos string) [][]string {
 	return result
 }
 
+// handles of course reversing lenition
 func Lenition(w string, w_re string) [][]string {
 
 	var result [][]string
