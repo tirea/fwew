5ae6611fa128b580906a2339f89646a61f13a678
Tirea Aean <tirea@learnnavi.org>
2016-07-03 13:48:04 -0700

    Create debug.go
    program will most likely not compile and run until this is fully integrated properly.

---
+++ util/debug.go
@@ -0,0 +1,38 @@
+//  This file is part of Fwew.
+//  Fwew is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Fwew is distributed in the hope that it will be usefil,
+// but WITHOUT ANY WARRANTY; without even implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+// GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Fwew.  If not, see http://gnu.org/licenses/
+
+// This util library handles all the debug probing output
+
+package util
+
+import (
+  "fmt"
+)
+
+var head string = "<DEBUG:"
+var mid string = ">"
+var tail string = "</DEBUG>"
+
+// output program state snapshots given function and variable names
+func Debug(progDebug bool, funcName string, varNames []string, varVals []string) {
+  if len(varNames != len(varVals) {
+    fmt.Println("Debugging error: varNames and varVals unequal size")
+    exit(1)
+  }
+  if progDebug {
+    for i := 0; i < len(varNames); i++ {
+      fmt.Println(head + funcName + varNames[i] + mid + varVals[i] + tail)
+    }
+  }
+}
