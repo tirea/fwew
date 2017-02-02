4c51c760310c7a458c6d632a8bc029c2f260f3ff
Tirea Aean <tirea@learnnavi.org>
2017-02-02 13:30:37 -0600

    Add license comment block

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+#	This file is part of Fwew.
+#	Fwew is free software: you can redistribute it and/or modify
+# 	it under the terms of the GNU General Public License as published by
+# 	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
+#
+#	Fwew is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with Fwew.  If not, see http://gnu.org/licenses/
 sudo cp ./bin/fwew /usr/local/bin && \
 echo "Fwew installed to /usr/local/bin/fwew" && \
 cp -r ./.fwew $HOME && \
 echo "Fwew data installed to $HOME/.fwew"
+echo "Installed Successfully"
