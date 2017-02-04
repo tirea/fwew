cb7a6d1bf1d4d73d7faf4942d7020cfa9e672c52
Tirea Aean <tirea@learnnavi.org>
2017-02-03 20:56:36 -0600

    GPL comment, change package name to test

--- test.go
+++ test.go
@@ -1,6 +1,19 @@
-// TODO
-// Testing
-package fwew-test
+//	This file is part of Fwew.
+//	Fwew is free software: you can redistribute it and/or modify
+// 	it under the terms of the GNU General Public License as published by
+// 	the Free Software Foundation, either version 3 of the License, or
+// 	(at your option) any later version.
+//
+//	Fwew is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with Fwew.  If not, see http://gnu.org/licenses/
+
+// Testing package
+package test
 
 import (
 	"fmt"
@@ -9,5 +22,5 @@ import (
 )
 
 func main() {
-	// Test stuff
+	// TODO Test stuff
 }
