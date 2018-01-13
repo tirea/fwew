778c819f8c54db60bea5551709f43e10674c0471
Tirea Aean <tirea@learnnavi.org>
2018-01-12 23:33:41 -0500

    Update dictionary

--- util/version.go
+++ util/version.go
@@ -25,7 +25,7 @@ type version struct {
 }
 
 // Version is a printable version struct containing program version information
-var Version = version{1, 14, 0, "dev", "Eana Yayo", "Na'vi Dictionary 13.61 (20 DEC 2017)"}
+var Version = version{1, 14, 0, "dev", "Eana Yayo", "Na'vi Dictionary 13.62 (05 JAN 2018)"}
 
 func (v version) String() string {
 	if v.Label != "" {
