ce5833600d2fbb43d5fe47ae50192176d3396aee
Tirea Aean <tirea@learnnavi.org>
2019-03-04 03:21:49 -0500

    finally start tests and CI

---
+++ affixes/affixes_test.go
@@ -0,0 +1,56 @@
+package affixes
+
+import "testing"
+
+func TestPrefix(t *testing.T) {
+	w0 := Word{
+		Target:       "tsatute",
+		Attempt:      "tute",
+		PartOfSpeech: "n.",
+		Affixes:      map[string][]string{},
+	}
+	w0 = prefix(w0)
+	if w0.Attempt != w0.Target {
+		t.Errorf("Got %s, Want %s", w0.Attempt, w0.Target)
+	}
+}
+
+func TestSuffix(t *testing.T) {
+	w0 := Word{
+		Target:       "eltuti",
+		Attempt:      "eltu",
+		PartOfSpeech: "n.",
+		Affixes:      map[string][]string{},
+	}
+	w0 = suffix(w0)
+	if w0.Attempt != w0.Target {
+		t.Errorf("Got %s, Want %s", w0.Attempt, w0.Target)
+	}
+}
+
+func TestInfix(t *testing.T) {
+	w0 := Word{
+		Target:         "täpeykiyevarängon",
+		Attempt:        "taron",
+		InfixLocations: "t<0><1>ar<2>on",
+		PartOfSpeech:   "vtr.",
+		Affixes:        map[string][]string{},
+	}
+	w0 = infix(w0)
+	if w0.Attempt != w0.Target {
+		t.Errorf("Got %s, Want %s", w0.Attempt, w0.Target)
+	}
+}
+
+func TestReconstruct(t *testing.T) {
+	w0 := Word{
+		Target:       "tolaron",
+		Attempt:      "taron",
+		PartOfSpeech: "vtr.",
+		Affixes:      map[string][]string{},
+	}
+	w0 = Reconstruct(w0)
+	if w0.Attempt != w0.Target {
+		t.Errorf("Got %s, Want %s", w0.Attempt, w0.Target)
+	}
+}
