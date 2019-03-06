a48e04286b2a41622a3139bd45a98800e89bd945
Tirea Aean <tirea@learnnavi.org>
2019-03-06 05:27:53 -0500

    add header comment

--- affixes_test.go
+++ affixes_test.go
@@ -1,3 +1,18 @@
+//	This file is part of Fwew.
+//	Fwew is free software: you can redistribute it and/or modify
+// 	it under the terms of the GNU General Public License as published by
+// 	the Free Software Foundation, either version 3 of the License, or
+// 	(at your option) any later version.
+//
+//	Fwew is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with Fwew.  If not, see http://gnu.org/licenses/
+
+// Package main contains all the things. affixes_test.go -- you guess it -- tests affixes.go functions.
 package main
 
 import "testing"
