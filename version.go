021f827048b354542b69e45b175f2277efbccaf9
Tirea Aean <tirea@learnnavi.org>
2019-07-14 03:09:16 -0400

    3.6.2-dev: fix eyä

--- version.go
+++ version.go
@@ -30,7 +30,7 @@ type version struct {
 
 // Version is a printable version struct containing program version information
 var Version = version{
-	3, 6, 1,
+	3, 6, 2,
 	"dev",
 	"Eana Yayo",
 	13.9,
