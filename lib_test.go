6aeaaf62074ba0d9236d3de7bec63d688bd6a1f1
Tirea Aean <tirea@learnnavi.org>
2019-07-14 03:06:45 -0400

    create stubs for lib tests

---
+++ lib_test.go
@@ -0,0 +1,55 @@
+package main
+
+import "testing"
+
+func TestContains(t *testing.T) {
+
+}
+
+func TestContainsStr(t *testing.T) {
+
+}
+
+func TestDeleteElement(t *testing.T) {
+
+}
+
+func TestDeleteEmpty(t *testing.T) {
+
+}
+
+func TestIndex(t *testing.T) {
+
+}
+
+func TestIndexStr(t *testing.T) {
+
+}
+
+func TestIsLetter(t *testing.T) {
+
+}
+
+func TestReverse(t *testing.T) {
+
+}
+
+func TestStripChars(t *testing.T) {
+
+}
+
+func TestValid(t *testing.T) {
+
+}
+
+func TestDownloadDict(t *testing.T) {
+
+}
+
+func TestGlob(t *testing.T) {
+
+}
+
+func TestSHA1Hash(t *testing.T) {
+
+}
