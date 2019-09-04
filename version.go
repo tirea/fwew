cebed931eb2a9c892200d403baefd3114d7924b0
Tirea Aean <tirea@learnnavi.org>
2019-09-04 04:29:45 -0400

    3.8.0-dev: unicode arrow, /version command

--- version.go
+++ version.go
@@ -30,7 +30,7 @@ type version struct {
 
 // Version is a printable version struct containing program version information
 var Version = version{
-	3, 7, 0,
+	3, 8, 0,
 	"dev",
 	"Eana Yayo",
 	14.1,
