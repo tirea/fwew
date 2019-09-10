44e736ab410e979f68654a7fb51b31510ab7a0a9
Tirea Aean <tirea@learnnavi.org>
2019-09-10 12:22:30 -0400

    3.8.1-dev: updacte dict hash on /update

--- version.go
+++ version.go
@@ -30,7 +30,7 @@ type version struct {
 
 // Version is a printable version struct containing program version information
 var Version = version{
-	3, 8, 0,
+	3, 8, 1,
 	"dev",
 	"Eana Yayo",
 	14.2,
