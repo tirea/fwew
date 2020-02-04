ccfc6bf0d4de569115fc3c468600022fdd99434e
Tirea Aean <tirea@learnnavi.org>
2020-02-04 14:53:30 -0500

    3.13.0-dev: fix accuracy of /list syllables

--- version.go
+++ version.go
@@ -30,7 +30,7 @@ type version struct {
 
 // Version is a printable version struct containing program version information
 var Version = version{
-	3, 12, 0,
+	3, 13, 0,
 	"dev",
 	"Eana Yayo",
 	14.3,
