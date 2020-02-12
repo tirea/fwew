3e6c5e3df1c988e377080aa5dfed5780a23bc90f
Tirea Aean <tirea@learnnavi.org>
2020-02-12 12:40:15 -0500

    eng->en, est->et by ISO 639-1

--- txt.go
+++ txt.go
@@ -183,7 +183,7 @@ func init() {
 	texts["author"] = "Tirea Aean"
 	Version.DictBuild = SHA1Hash(texts["dictionary"])
 	texts["header"] = fmt.Sprintf("%s\n%s\n", Version, texts["tip"])
-	texts["languages"] = "de, eng, est, fr, hu, nl, pl, ru, sv"
+	texts["languages"] = "de, en, et, fr, hu, nl, pl, ru, sv"
 	texts["POSFilters"] = "allvtr.n.num.pn.adv.adj.vin.v.inter.part.svin.adp.adv., n.vtrm.vim.conj.pn., sbd.n., intj.intj."
 	texts["POSFilters"] += "vtrm., vtr.part., intj.vin., svin.prop.n.affixvin., intj.dem.dem., n.sbd.n., adv."
 	texts["POSFilters"] += "adj., n.adj., adv.adj., intj.dem., pn.vtr., vin.adv., intj.pn., adv.ph.vin., vtr.adj.,  conj."
